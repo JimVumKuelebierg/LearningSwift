9ec81d722b4890962fcded38a333d42dcffc181e
JimVumKuelebierg <jim.reckel@icloud.com>
2024-12-21 18:42:27 +0100

    Initial Commit

---
+++ LearningSwift/LearningSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftApp.swift
+//  LearningSwift
+//
+//  Created by Jim Reckel on 21/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearningSwiftTests/LearningSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftTests.swift
+//  LearningSwiftTests
+//
+//  Created by Jim Reckel on 21/12/2024.
+//
+
+import Testing
+@testable import LearningSwift
+
+struct LearningSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
